5aae28d3475a06ae5deedf0af3580944ee80233f
Danny <sdanielabelvedere@gmail.com>
2019-11-24 18:41:38 -0300

    First attempt card with radio component

---
+++ src/Components/MovieCard/MovieCard.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import MovieDetails from '../MovieDetails/MovieDetails';
+
+class MovieCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.Movies = [
+      {
+        title: "The Joker",
+        img: "https://www.joblo.com/assets/images/joblo/posters/2019/08/joker-poster-main2.jpg",
+        description: "La peli del joker"
+      },
+      {
+        title: "Malefica",
+        img: "https://m.media-amazon.com/images/M/MV5BZjJiYTExOTAtNWU0Yi00NzJjLTkwOTgtOTU2NWM1ZjJmYWVhXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
+        description: "La peli de maleficient"
+
+      },
+      {
+        title: "Doctor Sleep",
+        img: "https://m.media-amazon.com/images/M/MV5BYmY3NGJlYTItYmQ4OS00ZTEwLWIzODItMjMzNWU2MDE0NjZhXkEyXkFqcGdeQXVyMzQzMDA3MTI@._V1_.jpg",
+        description: "Doctor sleep la peli"
+      },
+      {
+        title: "Downtown Abbey",
+        img: "https://ewedit.files.wordpress.com/2019/03/da_andy.jpg?w=1800&h=2650",
+        description: "Downtown Abbey, la película"
+      },
+      {
+        title: "Proyecto Geminis",
+        img: "https://m.media-amazon.com/images/M/MV5BNjI5OTNkMzUtZDYzYy00NWQ5LTg4YzYtZmZjZDI0MGQzNGY2XkEyXkFqcGdeQXVyNjg2NjQwMDQ@._V1_.jpg",
+        description: "Proyecto Geminis"
+      },
+      {
+        title: "Zombieland 2",
+        img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/zombieland-2-teaser-poster-1548837272.jpg",
+        description: "Zombieland 2"
+      },
+
+    ]
+  }
+  render() {
+    return (
+      <React.Fragment>
+
+        {this.Movies.map((movie, key) => {
+          return <MovieDetails
+            image={movie.img}
+            title={movie.title}
+            description={movie.description}
+            key={key}/>
+        })
+        }
+      </React.Fragment>
+    )
+
+  }
+
+}
+
+export default MovieCard;
---
+++ src/Components/MovieDetails/MovieDetails.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Radio from '@material-ui/core/Radio';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+    card: {
+        maxWidth: 250,
+        width: 250,
+        margin: 10,
+    },
+});
+
+class MovieDetails extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            selectedValue: '',
+        }
+    }
+
+    handleChange(e) {
+        const movie = e.target.value;
+        this.setState({
+            selectedValue: movie
+        })
+        console.log(movie);
+        console.log(this.state);
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <Card className={classes.card} >
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        alt=""
+                        height="200"
+                        image={this.props.image}
+                        title={this.props.title}
+                    />
+                    <CardContent>
+                        <Radio
+                            checked={this.state.selectedValue === this.props.title}
+                            value={this.props.title}
+                            name="movie-selection"
+                            onChange={e => this.handleChange(e)}
+                            inputProps={{ 'aria-label': this.props.title, 'aria-checked': this.state.selectedValue === this.props.title}}
+                        />
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {this.props.title}
+                        </Typography>
+                        <Typography variant="body2" color="textSecondary" component="p">
+                            {this.props.description}
+
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+                <CardActions>
+                    <Button size="small" color="primary">
+                        Ver Trailer
+                    </Button>
+                    <Button size="small" color="primary">
+                        Más info
+                    </Button>
+                </CardActions>
+            </Card>
+        )
+    }
+
+}
+
+export default withStyles(styles)(MovieDetails);
