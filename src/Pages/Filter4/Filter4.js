e95f054514c6be916b6a10212b8508de4a49ef0c
danibelve <sdanielabelvedere@gmail.com>
2019-12-08 02:24:42 -0300

    saco opcion

--- src/Pages/Filter4/Filter4.js
+++ src/Pages/Filter4/Filter4.js
@@ -85,13 +85,13 @@ class Filter4 extends React.Component {
     let opcionSeleccionada = "Sin definir";
   
       if (butacaValue === "0") {
-        opcionSeleccionada = "Opción 01. E4, E5, E6, E7";
+        opcionSeleccionada = "E4, E5, E6, E7";
       }
       if (butacaValue === "1") {
-        opcionSeleccionada = "Opción 02. F7, F8, F9, F10";
+        opcionSeleccionada = "F7, F8, F9, F10";
       }
       if (butacaValue === "2") {
-        opcionSeleccionada = "Opción 03. H6, H7, H8, H9";
+        opcionSeleccionada = "H6, H7, H8, H9";
       }
       if (butacaValue=== "3") {
         opcionSeleccionada = "Personalizado";
